280abc65be564296df1eb05470242eea29d010c6
williamdc92 <guglielmodc@gmail.com>
2022-03-14 22:05:27 +0100

    add control rotation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Storage } from '@ionic/storage-angular';
 import { UserService, CartElement, Me, orders } from './service/UserService/user-service';
 import { ToastController } from '@ionic/angular';
 import { MenuController } from '@ionic/angular';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 
 @Component({
   selector: 'app-root',
@@ -19,47 +20,65 @@ export class AppComponent {
     { title: 'Wishlist', url: '/wishlist', icon: 'heart' },
     { title: 'Order History', url: '/order-history', icon: 'archive' }
   ];
-  
-  
-  
-  
-  constructor(private menu: MenuController, public router: Router, public service: ShopService, private storage: Storage, public userService: UserService, public toastController: ToastController) {
+
+
+
+
+  constructor(private screenOrientation: ScreenOrientation, private menu: MenuController, public router: Router, public service: ShopService, private storage: Storage, public userService: UserService, public toastController: ToastController) {
   }
-  
+
   public producers = []
   public categories = [];
   database: RootObject[];
   cart: CartElement[];
   currentUser: Me;
 
-  isEmpty = true; 
+  isEmpty = true;
   isAdmin: boolean;
   total: number = 0;
-  
-  
-  async ngOnInit() {
-    await this.storage.create();
-    this.database = await this.service.GetDatabase();
-    this.GetProducerCategory()
 
-    await this.GetUser();
-    await this.GetCart()
-    
-    
+
+  async ngOnInit() {
+
+    try {
+      await this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+    }
+
+    catch {
+      console.log(" Rotation: Not in a mobile device")
+    }
+
+    try {
+
+      await this.storage.create();
+      this.database = await this.service.GetDatabase();
+      this.GetProducerCategory()
+
+      await this.GetUser();
+      await this.GetCart()
+    }
+
+    catch (err) {
+      console.log(err)
+    }
+
+
+
+
   }
-  
-  
+
+
   Refresh = async () => {
-    
+
     try {
       this.total = this.cart.map(item => item.tot).reduce((sum, item) => sum + item)
       this.isEmpty = false;
     }
 
-    catch(err) {
+    catch (err) {
       console.log(err)
     }
-    
+
 
     if (this.service.datachange == true) {
       this.database = await this.service.GetDatabase();
@@ -71,39 +90,39 @@ export class AppComponent {
       await this.GetCart()
       this.service.cartchange = false
     }
-    
+
     if (this.userService.cartfirstcheck == true) {
       await this.GetCart()
       this.userService.cartfirstcheck = false;
     }
-    
-    
-    if (this.userService.userfirstcheck ==true) {
-     await this.GetUser(); 
-     this.userService.userfirstcheck = false;
+
+
+    if (this.userService.userfirstcheck == true) {
+      await this.GetUser();
+      this.userService.userfirstcheck = false;
     }
-    
-    
-    
+
+
+
   }
-  
+
   GetProducerCategory = () => {
     this.producers = this.database.map((item: { producer: string; }) => item.producer).filter((item, pos, self) => { return self.indexOf(item) == pos; });
     this.categories = this.database.map((item: { category: string; }) => item.category).filter((item, pos, self) => { return self.indexOf(item) == pos; });
   }
-  
-  openEnd = () => {this.menu.close()}
-  
-  
+
+  openEnd = () => { this.menu.close() }
+
+
   GetUser = async () => {
-    
+
     if (await this.storage.get('logged') === true) {
       try {
         this.currentUser = await this.userService.GetMe(await this.storage.get('token'));
         this.isAdmin = this.currentUser?.isAdmin;
       }
-      
-      
+
+
       catch (err) {
         console.log(err)
         const toast = await this.toastController.create({
@@ -115,19 +134,19 @@ export class AppComponent {
       }
     }
   }
-  
+
   LogOut = async () => {
     await this.storage.set('logged', false);
     await this.storage.set('viewed', false);
     await this.storage.set('email', "");
     await this.storage.set('token', "");
     await this.storage.set('id', "");
-    
+
     this.userService.activeSessions = false;
     await this.router.navigate(['tutorial'])
   }
-  
-  
+
+
   GetCart = async () => {
     if (await this.storage.get('logged') === true) {
       try {
@@ -136,14 +155,14 @@ export class AppComponent {
         this.cart = await this.userService.GetCart(await this.storage.get('id'));
         this.total = this.cart.map(item => item.tot).reduce((sum, item) => sum + item)
       }
-      
+
       catch {
         this.isEmpty = true;
-        
+
       }
     }
   }
-  
+
   RemoveFromCart = async (idp: string) => {
     try {
       await this.userService.RemoveProductFromCart(await this.storage.get('id'), idp);
@@ -155,29 +174,29 @@ export class AppComponent {
       toast.present();
       this.service.cartchange = true;
       this.Refresh();
-      
+
     }
-    
+
     catch (err) {
       const toast = await this.toastController.create({
         message: `Cannot remove product from cart. Please try again`,
         duration: 2000
       });
       toast.present();
-      
+
     }
-    
+
   }
-  
+
   PlaceOrder = async () => {
-    
+
     const order: orders = {
       date: new Date,
       total: this.total,
       items: this.cart,
       id: ""
     }
-    
+
     try {
       await this.userService.AddOrder((await this.storage.get('id')), order, (await this.storage.get('token')))
       const toast = await this.toastController.create({
@@ -190,7 +209,7 @@ export class AppComponent {
       this.openEnd();
       this.total = 0;
     }
-    
+
     catch (err) {
       const toast = await this.toastController.create({
         message: `Sorry, can't place order`,
@@ -200,15 +219,15 @@ export class AppComponent {
       console.log(err);
     }
   }
-  
+
   IncreaseQuantity = async (idp: string) => {
-    
+
     try {
       await this.userService.IncreaseQuantity(await this.storage.get('id'), idp)
       this.service.cartchange = true;
       this.Refresh();
     }
-    
+
     catch (err) {
       const toast = await this.toastController.create({
         message: `Sorry, can't add element. Please try again later`,
@@ -218,15 +237,15 @@ export class AppComponent {
       console.log(err);
     }
   }
-  
+
   DecreaseQuantity = async (idp: string) => {
-    
+
     try {
       await this.userService.DecreaseQuantity(await this.storage.get('id'), idp)
       this.service.cartchange = true;
       this.Refresh();
     }
-    
+
     catch (err) {
       const toast = await this.toastController.create({
         message: `Sorry, can't remove element. Please try again later`,
@@ -236,6 +255,6 @@ export class AppComponent {
       console.log(err);
     }
   }
-  
-  
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 
 
 
@@ -16,7 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,IonicStorageModule.forRoot(),Ng2SearchPipeModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [ScreenOrientation,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
